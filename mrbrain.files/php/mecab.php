4fc3f21af3e3f84da71c59c27de2e64b1a50dac2
Tomoya Koyanagi <tomk79@gmail.com>
2013-05-25 00:56:32 +0900

    mecab を通して結果を出力するところまでできた。

--- mrbrain.files/php/mecab.php
+++ mrbrain.files/php/mecab.php
@@ -2,10 +2,33 @@
 
 class cont_mecab{
 	private $px;
+	private $path_mecab = '/usr/local/bin/mecab';
 
 	public function __construct($px){
 		$this->px = $px;
 	}
+
+	/**
+	 * 形態素解析	
+	 */
+	public function parse( $text ){
+		$cmd = 'echo '.t::data2text($text).' | '.$this->path_mecab.' -E ""';
+		ob_start();
+		passthru($cmd);
+		$mecab_answer = ob_get_clean();
+		$parsed_text = trim($mecab_answer);
+
+		$parsed_text = preg_split( '/\r\n|\r|\n/', $parsed_text );
+		$rtn = array();
+		foreach( $parsed_text as $word ){
+			$tmp = array();
+			list($tmp['word'],$tmp['property']) = preg_split('/\t/',$word);
+			$tmp['property'] = preg_split('/\,/',$tmp['property']);
+			array_push( $rtn, $tmp );
+		}
+
+		return $rtn;
+	}//parse()
 }
 
 ?>
